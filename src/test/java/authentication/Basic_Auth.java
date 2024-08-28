b2fb3dc1cabe5f12aa5c4843ba5b9f19bce22ae3
veera pulapakura <veera_ratna@yahoo.com>
2024-08-28 19:09:21 +0100

    Authentication scenarios added
    Authentication scenarios added.
    Basic, Preemptive, digest and Bearer token.

---
+++ src/test/java/authentication/Basic_Auth.java
@@ -0,0 +1,69 @@
+package authentication;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basic_Auth {
+
+@Test(priority =2)
+public void Authentication(){
+
+    given()
+            .auth().basic("postman","password")
+            .when()
+            .get("https://postman-echo.com/basic-auth")
+            .then()
+            .statusCode(200)
+            .body("authenticated",equalTo(true))
+            .log().all();
+            //Assert.assertEquals(res.getStatusCode(), 200);
+}
+
+    @Test(priority =2)
+    public void DigestAuthentication(){
+
+        given()
+                .auth().digest("postman","password")
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+                .then()
+                .statusCode(200)
+                .body("authenticated",equalTo(true))
+                .log().all();
+        //Assert.assertEquals(res.getStatusCode(), 200);
+    }
+
+
+    @Test(priority =3)
+    public void PreemptiveAuthentication(){
+
+        given()
+                .auth().preemptive().basic("postman","password")
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+                .then()
+                .statusCode(200)
+                .body("authenticated",equalTo(true))
+                .log().all();
+        //Assert.assertEquals(res.getStatusCode(), 200);
+    }
+
+
+    @Test(priority =3)
+    public void BearerTokenAuthentication()
+    {
+        String token = "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";
+        given()
+                .headers("Authorization", "Bearer "+token)
+                .when()
+                .get("https://api.github.com/user/repos")
+                .then()
+                .statusCode(200)
+                //.body("authenticated",equalTo(true))
+                .log().all();
+    }
+}
